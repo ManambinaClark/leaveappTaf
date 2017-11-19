176e3cc0873ede8652227a572a34f56fcc102936
ManambinaClark <manambinaclark2@gmail.com>
2017-11-19 22:25:54 +0300

    19-11-2017

---
+++ LeaveAppWPF/ViewModel/ListStaffLeaveViewModel.cs
@@ -0,0 +1,35 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System.Windows.Input;
+
+namespace LeaveAppWPF.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that a View can data bind to.
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class ListStaffLeaveViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the ListStaffLeaveViewModel class.
+        /// </summary>
+        public ListStaffLeaveViewModel()
+        {
+            GoBack = new RelayCommand(GoDashboard);
+        }
+
+        #region Methodes
+        private void GoDashboard()
+        {
+            ViewModelLocator.Main.CurrentView = ViewModelLocator.DashboardVM;
+        }
+        #endregion
+
+        #region Arguments
+        public ICommand GoBack { get; set; }
+
+        #endregion
+    }
+}
---
+++ LeaveAppWPF/ViewModel/LoginViewModel.cs
@@ -0,0 +1,35 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System.Windows.Input;
+
+
+namespace LeaveAppWPF.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that a View can data bind to.
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class LoginViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the LoginViewModel class.
+        /// </summary>
+        /// 
+        public MainWindow main;
+        public LoginViewModel()
+        {
+            Connect = new RelayCommand(Connexion);
+        }
+
+        private void Connexion()
+        {
+            main = new MainWindow();
+            main.Show();
+
+        }
+
+        public ICommand Connect { get; set; }
+    }
+}
